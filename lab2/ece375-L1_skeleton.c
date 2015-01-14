a0b05a6cb7c84ee9811e9d94d878135c17035044
Sean Rettig <seanrettig@gmail.com>
2015-01-14 09:38:05 -0800

    add lab2 files

---
+++ lab2/ece375-L1_skeleton.c
@@ -0,0 +1,28 @@
+
+/*
+This code will cause a TekBot connected to the AVR board to
+move forward and when it touches an obstacle, it will reverse
+and turn away from the obstacle and resume forward motion.
+
+PORT MAP
+Port B, Pin 4 -> Output -> Right Motor Enable
+Port B, Pin 5 -> Output -> Right Motor Direction
+Port B, Pin 7 -> Output -> Left Motor Enable
+Port B, Pin 6 -> Output -> Left Motor Direction
+Port D, Pin 1 -> Input -> Left Whisker
+Port D, Pin 0 -> Input -> Right Whisker
+*/
+
+#define F_CPU 16000000
+#include <avr/io.h> 
+#include <util/delay.h> 
+#include <stdio.h>
+
+int main(void)
+{
+
+while (1)	       					// Loop Forever
+      {
+	      // Your code goes here
+      };
+}
