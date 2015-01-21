6cf2a242690ce8a429f8599bd65cbeba8de21107
Sean Rettig <seanrettig@gmail.com>
2015-01-21 15:44:35 -0800

    fix lab2 code, now works

--- lab2/1.c
+++ lab2/1.c
@@ -19,30 +19,30 @@ Port D, Pin 0 -> Input -> Right Whisker
 
 int main(void)
 {
-	
-	DDRB =0b11110000;	//Set up Port B for Input/Output
-	PORTB=0b11110000;	//Turn off both motors
-	
-	DDRD =0b00000000;	//Set up Port D for Input/Output
-	PORTD=0b00000011;	//Set pullup resistors on
 
-    PORTB=0b01100000;	//Move forward
+    DDRB =0b11110000;   //Set up Port B for Input/Output
+    PORTB=0b11110000;   //Turn off both motors
 
-	while (1)	       					// Loop Forever
-	{
-		if(PIND & 0b00000011 == 0b00000001){ //Right whisker hit when PIND == 0bxxxxxx01
-			PORTB=0b00000000;	//Reverse
-			_delay_ms(1000);
-			PORTB=0b00100000; //Turn Left
-			_delay_ms(1000);
-            PORTB=0b01100000;	//Move forward
-		}
-		else if(PIND & 0b00000011 == 0b00000001 || PIND & 0b00000011 == 0b00000011){ //Left whisker hit or both whiskers hit at once
-			PORTB=0b00000000;	//Reverse
-			_delay_ms(1000);
-			PORTB=0b01000000; //Turn Right
-			_delay_ms(1000);	
-            PORTB=0b01100000;	//Move forward
-		}
-	};
-}
+    DDRD =0b00000000;   //Set up Port D for Input/Output
+    PORTD=0b00000011;   //Set pullup resistors on
+
+    PORTB=0b01100000;   //Move forward
+    
+    while (1)                           // Loop Forever
+        {
+            if((PIND | 0b11111100) == 0b11111110){ //Right whisker hit when PIND == 0bxxxxxx10
+                PORTB=0b00000000;   //Reverse
+                _delay_ms(1000);
+                PORTB=0b00100000; //Turn Left
+                _delay_ms(1000);
+                PORTB=0b01100000;   //Move forward
+            }
+            else if((PIND | 0b11111100) == 0b11111101 || (PIND | 0b11111100) == 0b11111100){ //Left whisker hit or both whiskers hit at once
+                PORTB=0b00000000;   //Reverse
+                _delay_ms(1000);
+                PORTB=0b01000000; //Turn Right
+                _delay_ms(1000);
+                PORTB=0b01100000;   //Move forward
+            }
+        };
+    }
